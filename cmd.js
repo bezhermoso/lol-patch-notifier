4ed0ac1e2453d3d36468ff8b06420aa7d81d6b98
Bez Hermoso <bezalelhermoso@gmail.com>
2015-10-02 14:14:18 -0700

    Cleanup + comments

--- cmd.js
+++ cmd.js
@@ -9,6 +9,32 @@ var Promise = require("bluebird");
 command.option('-s, --since <v>', 'Patch number (i.e. 5.19)');
 command.option('-f, --frequency <minutes>', 'Check & notification frequency');
 
+/** 
+ * Checks for any patches since `since` and pass it unto the notifier.
+ *
+ * Returns a function that when executed, will start another notify cycle which
+ * checks for patches since the latest patch.
+ */
+var notifyCycle = function (notifier, since) {
+    console.log("Checking for patch since " + since + "...");
+    return versions.getPatchesSince(since).then(function (patches) {
+        if (patches.length == 0) {
+            console.log("No patches since " + since + ". Will check for same patch...");
+            return function () {
+                return notifyCycle(notifier, since);
+            }
+        } else {
+            var latest = patches[0];
+            console.log(patches.length + " new patches! Notifying + will check for patch since " + latest);
+            notifier(patches, since);
+            return function () {
+                return notifyCycle(notifier, latest);
+            }
+        }
+    });
+}
+
+//TODO: Move notifier to notifiers/slack.js. Make notifiers register themselves to the CLI object.
 
 var slackNotifier = function (webhookUrl) {
     return function (patches, since) {
@@ -26,27 +52,9 @@ var slackNotifier = function (webhookUrl) {
     }
 }
 
-var notifyCycle = function (notifier, since) {
-    console.log("Checking for patch since " + since + "...");
-    return versions.getPatchesSince(since).then(function (patches) {
-        if (patches.length == 0) {
-            console.log("No patches since " + since + ". Will check for same patch...");
-            return function () {
-                return notifyCycle(notifier, since);
-            }
-        } else {
-            var latest = patches[0];
-            console.log(patches.length + " new patches! Notifying + will check for patch since " + latest);
-            notifier(patches, since);
-            return function () {
-                return notifyCycle(notifier, latest);
-            }
-        }
-    })
-}
+//TODO: Abstract notifyCycle + setInterval into a function that will be injected to notifiers.
 
-var slackCmd = command.command('slack <webhookUrl>')
-.action(function (webhookUrl, options) {
+var slackCmd = command.command('slack <webhookUrl>').action(function (webhookUrl, options) {
     var notifier = slackNotifier(webhookUrl);
     console.log("Starting check/notification cycle...");
     notifyCycle(notifier, command.since).then(function (exec) {
