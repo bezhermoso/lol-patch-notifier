9a2de84adaea22d6f851966a67ccc4cac1cf12d3
Bez Hermoso <bezalelhermoso@gmail.com>
2015-10-02 13:31:26 -0700

    Added frequency option

--- cmd.js
+++ cmd.js
@@ -26,20 +26,20 @@ var slackNotifier = function (webhookUrl) {
     }
 }
 
-var daemon = function (notifier, since) {
+var notifyCycle = function (notifier, since) {
     console.log("Checking for patch since " + since + "...");
     return versions.getPatchesSince(since).then(function (patches) {
         if (patches.length == 0) {
             console.log("No patches since " + since + ". Will check for same patch...");
             return function () {
-                return daemon(notifier, since);
+                return notifyCycle(notifier, since);
             }
         } else {
             var latest = patches[0];
             console.log(patches.length + " new patches! Notifying + will check for patch since " + latest);
             notifier(patches, since);
             return function () {
-                return daemon(notifier, latest);
+                return notifyCycle(notifier, latest);
             }
         }
     })
@@ -48,8 +48,8 @@ var daemon = function (notifier, since) {
 var slackCmd = command.command('slack <webhookUrl>')
 .action(function (webhookUrl, options) {
     var notifier = slackNotifier(webhookUrl);
-    console.log("Starting check/notification deamon...");
-    daemon(notifier, command.since).then(function (exec) {
+    console.log("Starting check/notification cycle...");
+    notifyCycle(notifier, command.since).then(function (exec) {
         setInterval(function () {
             exec().then(function (n) {
                 exec = n;
